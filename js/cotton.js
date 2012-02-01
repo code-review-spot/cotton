8fd2fe98d1e54a3af13feef35d1c923f690ac4c4
Deepak Jois <deepak.jois@gmail.com>
2012-01-31 21:34:51 -0800

    Basic thread list parsing

--- js/cotton.js
+++ js/cotton.js
@@ -1,6 +1,53 @@
-/* Author:
+(function() {
+  var Thread = Backbone.Model.extend({
+  });
 
-*/
+  var ThreadCollection = Backbone.Collection.extend({
+    model: Thread,
+
+    parse: function(response){
+      return response.results;
+    }
+  })
+
+  var Inbox = new ThreadCollection;
+  Inbox.url = "/samples/inbox.json";
+
+  var ThreadView = Backbone.View.extend({
+    tagName: "div",
+
+    // FIXME use hogan
+    template: _.template($('#thread-template').html()),
+
+    render: function() {
+      $(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    }
+  });
+
+  var AppView = Backbone.View.extend({
+    el: $("#cottonapp"),
+
+    initialize: function() {
+      Inbox.bind('add', this.addThread, this);
+      Inbox.bind("reset", this.addAllThreads, this);
+      Inbox.fetch();
+    },
+
+    addThread: function(thread) {
+      var view = new ThreadView({model: thread});
+      $("#cottonapp").append(view.render().el);
+    },
+
+    addAllThreads: function() {
+      Inbox.each(this.addThread);
+    }
+
+  });
+
+  window.App = new AppView;
+})();
+
 
 
 
